0c30037a7746496edaed9724a46ec0977f8246bd
Sushil Karkee <sushilkarkee@gmail.com>
2015-09-23 21:57:36 +0530

    jpt

--- CheckingGitHub/src/HelloWorld.java
+++ CheckingGitHub/src/HelloWorld.java
@@ -2,8 +2,7 @@
 public class HelloWorld {
 public static void main(String[] args) {
 
-	System.out.println("Hello GitHuiohb dfzfdAgain from Eclmmipses");
-
-	System.out.println("Hello GitHubhhiogeogegoe hoihi Again. at 21/9/2015.........");
+	
+	System.out.println("Hello once again");
 }
 }
