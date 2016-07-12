3662515ca0f63943dca69069e0c0761e7d6d5277
sushil.bahadur.karki <sushil.bahadur.karki@HDC4-D-3B7ZXC2.dir.svc.accenture.com>
2016-07-12 17:39:31 +0530

    second changeds

--- CheckingGitHub/src/HelloWorld.java
+++ CheckingGitHub/src/HelloWorld.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	
 	System.out.println("Hello once again from github from eclipse and github on the sth premises");
 	System.out.println("Greeting from the github");
+	System.out.println("This is from eclipse again");
 }
 }
