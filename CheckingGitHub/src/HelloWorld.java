2f3587e86bf368911adc8ed8f7e72f733bdca384
Sushil Karkee <sushilkarkee@users.noreply.github.com>
2016-07-12 17:51:52 +0530

    Update HelloWorld.java

--- CheckingGitHub/src/HelloWorld.java
+++ CheckingGitHub/src/HelloWorld.java
@@ -6,5 +6,7 @@ public static void main(String[] args) {
 	System.out.println("Hello once again from github from eclipse and github on the sth premises");
 	System.out.println("Greeting from the github");
 	System.out.println("This is from eclipse again");
+	System.out.println("This is from github again");
+
 }
 }
