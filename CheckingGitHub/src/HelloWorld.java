ecac788d21160d4484bf3b0e1846fee2050d5311
sushil.bahadur.karki <sushil.bahadur.karki@HDC4-D-3B7ZXC2.dir.svc.accenture.com>
2016-07-13 15:38:52 +0530

    file contents deleted

--- CheckingGitHub/src/HelloWorld.java
+++ CheckingGitHub/src/HelloWorld.java
@@ -3,11 +3,7 @@ public class HelloWorld {
 public static void main(String[] args) {
 
 	
-	System.out.println("Hello once again from github from eclipse and github on the sth premises");
-	System.out.println("Greeting from the github");
-	System.out.println("This is from eclipse again");
-	System.out.println("This is from github again");
-	System.out.println("This is again eclipse");
+	
 
 }
 }
